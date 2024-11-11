7a7f516c0a967e3a1f9c4c14d847930d16a1fa5b
Nino2502 <ninoleal2469@gmail.com>
2024-11-11 09:57:30 -0600

    Se modifico la estructura organizacional

--- src/app/pages/estructura-organizacional/estructura-organizacional.page.ts
+++ src/app/pages/estructura-organizacional/estructura-organizacional.page.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+
+import axios from 'axios';
+import qs from 'qs';
+
+
+
 @Component({
   selector: 'app-estructura-organizacional',
   templateUrl: './estructura-organizacional.page.html',
@@ -7,9 +13,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EstructuraOrganizacionalPage implements OnInit {
 
+  data_estructura: any[] = [];
+  errorMessage: string = '';
+
+
+
   constructor() { }
 
   ngOnInit() {
+
+    this.get_estructura()
+
+  }
+
+  async get_estructura(){
+    try{
+
+      const response = await axios.get('http://localhost/quicky_coffee/proyecto_escuela/Estructura/estructura_info');
+
+      this.data_estructura = response.data;
+
+      console.log("Soy empleados . ", this.data_estructura);
+
+
+    }catch(error){
+      this.errorMessage = 'Error fetching data';
+      console.log(error);
+
+    }
   }
 
 }
