f10531a1e845bdf8509d3a5101c024c0a6d94f68
Nino2502 <ninoleal2469@gmail.com>
2025-03-20 01:02:32 -0600

    Se agrego cosas

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.controlgerencias.com',
+  appId: 'com.controlgerencias.gerco',
   appName: 'Control-gerencias',
   webDir: 'www',
   bundledWebRuntime: false,
